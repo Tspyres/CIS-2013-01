2a4617f8c75218f585ef3cc9a938502a6c517db9
Tspyres <60015803+Tspyres@users.noreply.github.com>
2020-02-03 13:41:06 -0600

    Delete mpg.js

--- mpg.js
+++
@@ -1,7 +0,0 @@
- var floatMiles, floatGallons, floatMpg;
-     floatMiles = prompt("Enter miles driven");
-     miles = parseFloat(miles);
-     floatGallons = prompt("Enter gallons of gas used");
-     gallons = parseFloat(gallons);
-     floatMpg = floatMiles/floatGallons;
-     alert("Miles per gallon = " + floatMpg);
